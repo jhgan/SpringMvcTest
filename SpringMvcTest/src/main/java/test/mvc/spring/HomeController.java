ea5b7ed522d50d4aea6aff06a3f413bd976cfc39
jhgan <jhgan@st-company.net>
2016-03-17 11:48:47 +0900

    Revert "테스트"
    This reverts commit 7512d04f934de6efdef08e832f95d74491fb3f88.

--- SpringMvcTest/src/main/java/test/mvc/spring/HomeController.java
+++ SpringMvcTest/src/main/java/test/mvc/spring/HomeController.java
@@ -6,31 +6,25 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+/**
+ * Handles requests for the application home page.
+ */
 @Controller
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	@Value("${test.txt}")
-	private String temp;
-	
-	@Autowired
-	private Environment env;
-	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		logger.info("temp value : " + temp);
-		logger.info("temp value: " + env.getProperty("test.txt"));
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
